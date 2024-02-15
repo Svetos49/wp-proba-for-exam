bb64c5326b43e564a5445cf4d72277b3f3c2a63d
Svetos49 <svetivslav@gmail.com>
2024-02-15 17:19:29 +0200

    Theme add template parts files, ajust the header and index.php files

--- wp-content/themes/NewTheme/functions.php
+++ wp-content/themes/NewTheme/functions.php
@@ -5,7 +5,7 @@
  */
 
 function gitproba_assets() {
-  wp_enqueue_style( 'gitproba',  get_stylesheet_directory_uri() . '/assets/css/master.css', array(), '1.0');
+  wp_enqueue_style( 'gitproba',  get_stylesheet_directory_uri() . '/assets/css/master.css', array(), filemtime( get_template_directory() . '/assets/css/master.css' ));
 }
 
 add_action( 'wp_enqueue_scripts', 'gitproba_assets' );
--- wp-content/themes/NewTheme/header.php
+++ wp-content/themes/NewTheme/header.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo('charset'); ?>;">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Properties</title>
+	<title><?php wp_title(); ?></title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 
 	<!-- <link rel="stylesheet" href="http://localhost/www/GitProba/wp-content/themes/NewTheme/assets/css/master.css"> -->
 	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-  <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
+
 <body>
 	<div class="site-wrapper">
 		<header class="site-header">
-			<h1 class="site-title"><a href="#">Properties Offers</a></h1>
+			<?php if (is_home()) : ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url(get_home_url()); ?>">Properties Offers</a>
+				</h1>
+			<?php else : ?>
+				<p class="site-title">
+					<a href="<?php echo esc_url(get_home_url()); ?>">Properties Offers</a>
+				</p>
+			<?php endif; ?>
+			
 		</header>
